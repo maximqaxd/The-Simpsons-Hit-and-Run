e67bde7ba85ec5db550478debacba1c395c23255
ZenoArrows <129334871+ZenoArrows@users.noreply.github.com>
2023-08-16 11:05:58 +0100

    Use a single simpsonsUint64 definition
    [git-p4: depot-paths = "//depot/game/": change = 136]

--- code/main/globaltypes.h
+++ code/main/globaltypes.h
@@ -15,21 +15,8 @@
 //
 // define 64 bit integer for all platforms
 //
-#ifdef RAD_PS2
-typedef unsigned long simpsonsUInt64;
-#endif
-
-#ifdef RAD_GAMECUBE
-typedef unsigned long long simpsonsUInt64;
-#endif
-
-#if defined RAD_XBOX
-typedef unsigned __int64 simpsonsUInt64;
-#endif
-
-#if defined RAD_PC
 #include <cstdint>
+
 typedef uint64_t simpsonsUInt64;
-#endif
 
 #endif //GLOBALTYPES_H
