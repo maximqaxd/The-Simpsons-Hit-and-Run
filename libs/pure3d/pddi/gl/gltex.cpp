eedc112299a889d9eef0323ba6c2400865d8c0fe
ZenoArrows <129334871+ZenoArrows@users.noreply.github.com>
2024-04-07 03:40:04 +0200

    Don't assume compressed textures are supported on GLES

--- libs/pure3d/pddi/gl/gltex.cpp
+++ libs/pure3d/pddi/gl/gltex.cpp
@@ -32,7 +32,7 @@ static inline GLenum PickPixelFormat(pddiPixelFormat format)
     case PDDI_PIXEL_LUM8: return GL_LUMINANCE8;
     case PDDI_PIXEL_DUDV88: return GL_LUMINANCE8_ALPHA8;
 #endif
-#if defined(RAD_VITA) && defined(RAD_GLES)
+#if defined(RAD_GLES)
     case PDDI_PIXEL_DXT1: return GL_RGBA;
     case PDDI_PIXEL_DXT3: return GL_RGBA;
     case PDDI_PIXEL_DXT5: return GL_RGBA;
@@ -93,7 +93,7 @@ static inline pddiPixelFormat PickPixelFormat(pddiTextureType type, int bitDepth
     return PDDI_PIXEL_UNKNOWN;
 };
 
-#if defined( RAD_VITA ) && defined( RAD_GLES )
+#if defined(RAD_GLES)
 #include "decompress.h"
 #endif
 
@@ -117,7 +117,7 @@ void pglTexture::SetGLState(void)
 //      if(nMipMap == 0)
         if (type == PDDI_TEXTYPE_DXT1 || type == PDDI_TEXTYPE_DXT3 || type == PDDI_TEXTYPE_DXT5)
         {
-#if defined(RAD_VITA) && defined(RAD_GLES)
+#if defined(RAD_GLES)
             unsigned char* image = new unsigned char[xSize * ySize * 4];
             unsigned int blocksize = lock.format == PDDI_PIXEL_DXT1 ? 8 : 16;
             if (type == PDDI_TEXTYPE_DXT1)
